b9e19b39f61df8284a70abe32f6b80d480b765ca
alex0112 <alarsen0112@gmail.com>
2018-02-08 21:39:57 -0700

    Stubbed basic tests

---
+++ spec/currency_spec.rb
@@ -0,0 +1,40 @@
+require 'rspec'
+require_relative '../lib/currency.rb'
+
+describe Currency do
+  describe '#crypto_amount' do
+  end
+
+  describe '#usd_invested' do
+  end
+
+  describe '#usd_lost' do
+  end
+
+  describe '#usd_gained' do
+  end
+
+  describe '#usd_current_price' do
+  end
+
+  describe '#my_account_usd_price' do
+  end
+  
+  describe '#positive_roi?' do
+
+    context 'given a negative number' do
+      it 'returns false' do
+      end
+    end
+
+    context 'given a positive number' do
+      it 'returns true do' do
+      end
+    end
+    
+  end
+
+  describe '#calc_roi' do
+  end
+end
+
