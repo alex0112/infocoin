aa6766eb0571731eafdfb44698da333ea6fcd796
alex0112 <alarsen0112@gmail.com>
2018-02-08 23:45:37 -0700

    Stubbed data and behaviour in currency class.

--- lib/currency.rb
+++ lib/currency.rb
@@ -1,20 +1,61 @@
-class Currency
-  def initialize(symbol, client)
-    raise ArgumentError 'Must specify currency symbol (BTC BCH LTC ETH)' if nil? symbol
-    raise ArgumentError 'Must specify currency symbol (BTC BCH LTC ETH)' if nil? client
+#!/usr/bin/env ruby
+require 'coinbase/wallet'
+require 'figaro'
 
-    attr_accessor :usd_invested
-    @usd_invested = 5 #client
+class Currency
+  def initialize(symbol)
+    raise ArgumentError 'Must specify currency symbol (BTC BCH LTC ETH)' if nil? symbol || !([:BTC, :LTC, :BCH, :LTC].include?(symbol))
+    attr_reader: :symbol
+
+    @symbol                   = symbol
+    @api_client               = Coinbase::Wallet::Client.new(api_key:    ENV[coinbase_api_key],
+                                                            api_secret: ENV[coinbase_api_secret],
+                                                            CB_VERSION: '2018-02-08')
+    @account                  = @api_client.accounts[symbol]
+    @crypto_amount_in_wallet  = @account.balance
+    @usd_invested             = ''
+    @usd_lost                 = ''
+    @usd_gained               = ''
+    @crypto_current_usd_price = ''
+    @my_account_amound_usd    = ''
   end
+
+  def crypto_amount_in_wallet
+    self.api_client.refresh!
+    return @account.balance
+  end
+
+  def usd_invested
+    return @usd_invested
+  end
+
+  def usd_gained
+    return @usd_gained
+  end
+
+  def usd_lost
+    return @usd_lost
+  end
+
+  def crypto_current_usd_price
+    return @crypto_current_usd_price
+  end
+
+  def my_account_amound_usd
+    return @my_account_amound_usd
+  end
+  
 
   def positive_roi?
     roi = calc_roi()
-    roi.positive? ? true : false
+    roi.positive? ? return true : return false
   end
-
+  
   def calc_roi
-    currency = self.currency
-    return self.usd_invested - current_exchange_rate(self.symbol)
+    #self.usd_invested - current_exchange_rate(self.symbol)
   end
 
+  def current_exchange_rate
+  end
+  
 end
